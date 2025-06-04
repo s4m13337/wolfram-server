15454b9df4c78420523a1ecdd602230d2a653693
s4m13337 <sambeet96@gmail.com>
2025-06-04 22:20:36 +0200

    Removing clients

--- clients/R/r-client.r
+++
@@ -1,9 +0,0 @@
-library(httr)
-
-r <- POST('http://127.0.0.1:5858', body = 'DateString[]' )
-
-print(content(r))
-
-r <- POST('http://127.0.0.1:5858', body = 'RandomVariate[ NormalDistribution[], 3]' )
-
-print(content(r))
--- clients/python/python-client.py
+++
@@ -1,9 +0,0 @@
-import sys
-import requests
-
-url = 'http://127.0.0.1:5858'
-
-r = requests.post(url=url, data=sys.argv[1])
-
-print('result: ' + r.text)
-
